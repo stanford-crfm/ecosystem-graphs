3cc8f97f07cfed5b7d6ce8c357d14ac0cf8d4380
Dilara Soylu <dilarasoylu@users.noreply.github.com>
2022-04-04 09:54:08 -0700

    Add pre-commit.sh

---
+++ pre-commit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script fails when any of its commands fail.
+set -e
+
+#if ! [ -e venv ]; then
+#  python3 -m pip install virtualenv
+#  python3 -m virtualenv -p python3 venv
+#fi
+#
+#venv/bin/pip install -r requirements.txt
+#venv/bin/pip check
+
+# YAML style checks and linting
+echo ""
+echo "Checking for YAML formatting."
+venv/bin/yamllint . || (
+  echo ""
+  echo "YAML formatting failed. Fix the errors and run the check again."
+  echo ""
+  exit 1
+)
+echo "YAML formatting succeeded!"
+echo ""
+
+echo "Done."
