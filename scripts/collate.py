e83820849cb4b03910fead5e3229f43f7537e89e
Thomas Buhrmann <thomas@graphext.com>
2024-02-08 14:54:52 +0100

    Action to auto-collate all assets into single CSV.

---
+++ scripts/collate.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+import yaml
+from pandas import DataFrame
+
+
+ASSET_PATH = Path("./assets")
+
+
+def scalar(val):
+    """Clean scalar values whenever possible."""
+    if not isinstance(val, dict):
+        return val
+
+    if "explanation" in val:
+        return val.get("value")
+
+    if not val:
+        return None
+
+    return val
+
+
+def collate_assets():
+    """Store all assets together in a single tabular CSV file."""
+    paths = ASSET_PATH.glob("*.yaml")
+    assets = [asset for fp in paths for asset in yaml.safe_load(open(fp))]
+    df = DataFrame.from_records(assets)
+
+    # Cleanup missing data representations
+    df = df.replace("none", None)
+
+    # Make data consistent within columns (cannot have scalar and dicts in same column)
+    df = df.apply(lambda ser: ser.apply(scalar))
+
+    df.to_csv("./resources/all_assets.csv", index=False)
+
+
+if __name__ == "__main__":
+    collate_assets()
